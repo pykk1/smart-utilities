9cbf3f8d4a5ccef2ac0a0ef6a97b83a8e2739ad5
DESKTOP-F6S5DSQ\PyKK <pvlrazvan@gmail.com>
2023-06-22 17:45:45 +0300

    SMUT-08 add BadRequestException handling

--- src/main/java/org/smart/utilities/controllers/ControllerExceptionHandler.java
+++ src/main/java/org/smart/utilities/controllers/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.smart.utilities.controllers;
 
+import org.smart.utilities.exceptions.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -13,6 +14,11 @@ public class ControllerExceptionHandler {
   public ResponseEntity<String> handleAuthenticationCredentialsNotFoundException(
       AuthenticationException ex) {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You have to login");
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
   }
 }
 
